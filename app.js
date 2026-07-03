bf6b9f59dbc5da406b9e07e5fc99851c783c4e17
Ameny Chaouachi <130348160+amanitek@users.noreply.github.com>
2026-07-03 23:30:20 +0100

    Update WhatsApp number in app.jst test

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const products = [
 
 // WhatsApp Config - Placeholder for the user's phone number
 // Format: country code without + or 00. Example: '21650000000' for Tunisia
-const SELLER_WHATSAPP_NUMBER = "YOUR_PHONE_NUMBER"; // Ex: "21650123456"
+const SELLER_WHATSAPP_NUMBER = "21629208687"; // Ex: "21650123456"
 
 // Cart State
 let cart = [];
